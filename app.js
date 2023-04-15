3d833f1488fc14b154c22d2896cda73037109be1
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-04-15 22:38:29 +0300

    list crew contacts

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+//BACKGROUND CHANGE WITH USER THEME INPUT
+const tripTypeSelect = document.getElementById("trip-type");
+
+tripTypeSelect.addEventListener("change", function() {
+  const tripType = tripTypeSelect.value;
+  
+  if (tripType === "beach") {
+    document.body.style.backgroundImage = "url('')";
+    document.body.style.color = "#000000";
+  } else if (tripType === "nature") {
+    document.body.style.backgroundImage = "url('')"
+  } else if (tripType === "adventure") {
+    document.body.style.backgroundImage = "url('')"
+  } else if (tripType === "city") {
+    document.body.style.backgroundImage = "url('')"
+  } else if (tripType === "family") {
+    document.body.style.backgroundImage = "url('')"
+  } else if (tripType === "romantic") {
+    document.body.style.backgroundImage = "url('')"
+  } else {
+    document.body.style.backgroundImage = "";
+    document.body.style.backgroundColor = "";
+    document.body.style.color = "";
+  }
+});
+
 //DISPLAY THE NUMBER OF DAYS
 const startInput = document.getElementById("start");
 const endInput = document.getElementById("end");
@@ -10,3 +36,66 @@ endInput.addEventListener("change", () => {
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
   numDaysInput.value = diffDays;
 });
+
+//THE CREW
+
+const contacts = [];
+
+const addContactButton = document.getElementById("add-contact-btn");
+addContactButton.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  const crewName = document.getElementById("crew-name").value;
+  const phone = document.getElementById("phone").value;
+  const emergencyName = document.getElementById("emergency-name").value;
+  const emergencyPhone = document.getElementById("emergency-phone").value;
+
+  const newContact = {
+    crewName: crewName,
+    phone: phone,
+    emergencyName: emergencyName,
+    emergencyPhone: emergencyPhone
+  };
+
+  contacts.push(newContact);
+
+  const contactList = document.getElementById("contact-list");
+  const listItems = contacts.map(contact => `<li>${contact.crewName} - ${contact.phone} - ${contact.emergencyName} - ${contact.emergencyPhone}</li>`).join("");
+  contactList.innerHTML = listItems;
+
+  document.getElementById("crew-name").value = "";
+  document.getElementById("phone").value = "";
+  document.getElementById("emergency-name").value = "";
+  document.getElementById("emergency-phone").value = "";
+});
+
+
+var button = document.getElementById("generate-pdf");
+  button.addEventListener("click", function () {
+      var doc = new jsPDF("p", "mm", [300, 300]);
+      //PAGE TITLE
+      var pageTitle = document.querySelector("#page-title h3").textContent;
+      //TRIP INFO
+      var tripName =  $('input[name="trip-name"]').val();
+      var tripDestination = "Destination: " + $('input[name="destination"]').val();
+      var numPeople = "Number of people: " + $('input[name="num-people"]').val();
+      var typeOfTrip = "Type of trip " + $('input[name="trip-type"]').val();
+      //TRIP DATE
+      var tripDuration = "Trip duration (days): " + $('input[name="NumDays"]').val();
+      var tripStart = "Start date: " + $('input[name="trip-start"]');
+      var tripEnd = "End date: " + $('input[name="trip-end"]');
+      //THE CREW
+      //PAGE TITLE
+      doc.text(pageTitle, 20, 20);
+      //TRIP INFO
+      doc.text(tripName, 20, 30);
+      doc.text(tripDestination, 20, 40);
+      doc.text(numPeople, 20, 50);
+      doc.text(typeOfTrip, 20, 60)
+      //TRIP DURATION
+      doc.text(tripDuration, 20, 70);
+      doc.text(tripStart, 20, 80)
+      doc.text(tripEnd, 20, 90)
+      //THE CREW
+      doc.save("outOfTheGroupChat.pdf");
+  });
