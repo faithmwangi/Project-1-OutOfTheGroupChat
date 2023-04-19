db5aa9c25604d845f7b95c50c2788513b65a96d4
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-04-19 05:24:44 +0300

    complete

---
+++ public/README.md
@@ -0,0 +1,73 @@
+# Project-1-OutOfTheGroupChat
+
+### Faith Mwangi : Faith Mwangi, April 2023
+****
+## Project Description
+An app to help you and your group prioritise and plan your trip. Helping you save time, plan more effectively, and provide an organised format for all the important details of your trip
+******
+
+## SCREENSHOTS
+<img width="1426" alt="Screenshot 2023-04-19 at 05 17 44" src="https://user-images.githubusercontent.com/126608910/232949820-6ac830b6-9cce-4700-a70a-a8d746f271cd.png">
+<img width="1426" alt="Screenshot 2023-04-19 at 05 17 53" src="https://user-images.githubusercontent.com/126608910/232949857-a3866f06-21e3-4150-826a-654b9cf08efa.png">
+<img width="1426" alt="Screenshot 2023-04-19 at 05 17 59" src="https://user-images.githubusercontent.com/126608910/232949912-bafa4591-ccb1-4552-bb38-cc2ff9d6caa6.png">
+<img width="1426" alt="Screenshot 2023-04-19 at 05 18 06" src="https://user-images.githubusercontent.com/126608910/232949950-050d7ef3-69fa-4f77-9195-01a2f4504e6d.png">
+<img width="1426" alt="Screenshot 2023-04-19 at 05 18 14" src="https://user-images.githubusercontent.com/126608910/232949975-41629960-a49f-4fa3-bf99-066f653a384a.png">
+<img width="1426" alt="Screenshot 2023-04-19 at 05 18 20" src="https://user-images.githubusercontent.com/126608910/232950055-9ed16ad3-26a4-4220-8246-d6ecea7431f3.png">
+<img width="1426" alt="Screenshot 2023-04-19 at 05 18 27" src="https://user-images.githubusercontent.com/126608910/232950064-24aecef2-49e9-4b04-942e-0370c36dd9d9.png">
+<img width="1426" alt="Screenshot 2023-04-19 at 05 18 34" src="https://user-images.githubusercontent.com/126608910/232950126-af4037a9-1ce6-4b7c-9e1b-861a197757e7.png">
+<img width="1426" alt="Screenshot 2023-04-19 at 05 18 40" src="https://user-images.githubusercontent.com/126608910/232950127-d0aab2ef-b969-4c14-92b0-f27edc655dcd.png">
+<img width="1426" alt="Screenshot 2023-04-19 at 05 18 47" src="https://user-images.githubusercontent.com/126608910/232950166-f4a591fa-5e3a-4d18-ab6e-9a5892dc16ed.png">
+<img width="1426" alt="Screenshot 2023-04-19 at 05 18 55" src="https://user-images.githubusercontent.com/126608910/232950172-6e75c023-09df-4e87-8ed9-4abc61a566f1.png">
+<img width="1426" alt="Screenshot 2023-04-19 at 05 19 04" src="https://user-images.githubusercontent.com/126608910/232950187-2a378b99-0ab2-49db-94f4-9596f6121b29.png">
+<img width="1426" alt="Screenshot 2023-04-19 at 05 19 27" src="https://user-images.githubusercontent.com/126608910/232950208-63ddc0eb-ccdf-4f3a-9a63-66b1ab3aed68.png">
+<img width="1426" alt="Screenshot 2023-04-19 at 05 19 20" src="https://user-images.githubusercontent.com/126608910/232950198-72fa7e12-0f5f-44cd-ba63-db01ac5ea892.png">
+
+********
+## SetUp Instruction
+### Requirements
+* [Lang](lang Url)
+* [Framework](Framework URL)
+* Text editor eg [Visual Studio Code](https://code.visualstudio.com/download)
+* [Testing Platform](Platform URL)
+
+
+### Getting Files
+* Fork the repo
+- Create a new branch in your terminal (git checkout -b improve-feature)
+- Install the prerequisites
+- Make appropriate changes in file(s)
+- Run the server to see the changes
+- Add the changes and commit them (git commit -am "Improve App")
+- Push to the branch (git push origin improve-app)
+- Create a Pull request
+* Open the folder location on terminal and use the following command to run app:
+
+## How To Run It
+>  $ pip install -r requirements.txt
+To install all requirements
+
+> $ python3 manage.py runserver
+Then run server with the above command
+*****
+## Live Link
+[https://outofthegroupchat.web.app](https://outofthegroupchat.web.app)
+*****
+## Dependencies
+- Package Name
+- Package Name
+- Package Name
+- Package Name
+*****
+## Technologies Used
+1. HTML
+2. MdBootstrap
+3. HTML
+4. CSS
+5. JavaScript
+*****
+## Contact Information
+* Email : muthoninduta@gmail.com
+*****
+## [MIT LICENSE](LICENSE)
+MIT License
+Copyright (c) 2023 Faith Mwangi
---
+++ public/app.js
@@ -0,0 +1,231 @@
+
+//DISPLAY THE NUMBER OF DAYS
+const startInput = document.getElementById("start");
+const endInput = document.getElementById("end");
+const numDaysInput = document.getElementById("NumDays");
+
+endInput.addEventListener("change", () => {
+  const startDate = new Date(startInput.value);
+  const endDate = new Date(endInput.value);
+  const diffTime = Math.abs(endDate - startDate + 1);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  numDaysInput.value = diffDays;
+});
+
+//CONTACT INFO
+
+const contacts = [];
+
+const addContactButton = document.getElementById("add-contact-btn");
+addContactButton.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  const crewName = document.getElementById("crew-name").value;
+  const phone = document.getElementById("phone").value;
+  const emergencyName = document.getElementById("emergency-name").value;
+  const emergencyPhone = document.getElementById("emergency-phone").value;
+
+  const newContact = {
+    crewName: crewName,
+    phone: phone,
+    emergencyName: emergencyName,
+    emergencyPhone: emergencyPhone
+  };
+
+  contacts.push(newContact);
+
+  const contactList = document.getElementById("contact-list");
+  const listItems = contacts.map(contact => `<li>${contact.crewName} - ${contact.phone} - ${contact.emergencyName} - ${contact.emergencyPhone}</li>`).join("");
+  contactList.innerHTML = listItems;
+
+  document.getElementById("crew-name").value = "";
+  document.getElementById("phone").value = "";
+  document.getElementById("emergency-name").value = "";
+  document.getElementById("emergency-phone").value = "";
+});
+
+
+
+//GENERATE PDF
+var button = document.getElementById("generate-pdf");
+  button.addEventListener("click", function () {
+      var doc = new jsPDF("p", "mm", [300, 300]);
+      //PAGE TITLE
+      var pageTitle = document.querySelector("#page-title h3").textContent;
+      //TRIP INFO
+      var tripName =  $('input[name="trip-name"]').val();
+      var tripDestination = "Destination: " + $('input[name="destination"]').val();
+      var numPeople = "Number of people: " + $('input[name="num-people"]').val();
+      var typeOfTrip = "Trip Type: " + $('input[name="trip-type"]').val();
+      //TRIP DATE
+      var tripDuration = "Trip duration (days): " + $('input[name="NumDays"]').val();
+      var tripStart = "Start date: " + $('input[name="trip-start"]').val();
+      var tripEnd = "End date: " + $('input[name="trip-end"]').val();
+      // THE CREW
+      var contactsList = "";
+      for (var i = 0; i < contacts.length; i++) {
+        contactsList += contacts[i].crewName + " - " + contacts[i].phone + "\n" + "Emergency Contact: " + contacts[i].emergencyName + " - " + contacts[i].emergencyPhone + "\n" + "\n";
+      }
+      //TRANSPORTATION BOOKING
+      var transportGoing = "Going by: " + $('input[name="transport"]:checked').val();
+      var transportBooked = "Booked: " + $("#bGoing").val();
+      var transportReturn = "Returning by: " + $('input[name="transport-2"]:checked').val();
+      var returnBooked = "Booked: " + $("#bReturn").val();
+      //BUDGET
+      var budget = "Trip Budget: Ksh " + $('input[name="budget"]').val();
+      var transportCost = "Transport: Ksh " + $('input[name="t-cost"]').val();
+      var accomodationCost = "Accomodation: Ksh " + $('input[name="a-cost"]').val();
+      var groceriesCost = "Groceries: Ksh " + $('input[name="g-cost"]').val();
+      var drinksCost = "Drinks: Ksh " + $('input[name="d-cost"]').val();
+      var entertainmentCost = "Entertainment: Ksh " + $('input[name="e-cost"]').val();
+      //CALCULATE COST
+      const numOfPeople = document.getElementById('num-people').value;
+
+      const budgetInput = document.getElementById('budget').value;
+      const costPerPerson = budgetInput / numOfPeople;
+
+      const transportInputPerPerson = document.getElementById('t-cost').value;
+      const transportCostPerPerson = transportInputPerPerson / numOfPeople;
+
+      const accomodationInputPerPerson = document.getElementById('a-cost').value;
+      const accomodationCostPerPerson = accomodationInputPerPerson / numOfPeople;
+
+      const groceriesInputPerPerson = document.getElementById('g-cost').value;
+      const groceriesCostPerPerson = groceriesInputPerPerson / numOfPeople;
+
+      const drinksInputPerPerson = document.getElementById('d-cost').value;
+      const drinksCostPerPerson = drinksInputPerPerson / numOfPeople;
+
+      const entertainmentInputPerPerson = document.getElementById('a-cost').value;
+      const entertainmentCostPerPerson = entertainmentInputPerPerson / numOfPeople;
+
+      //ACCOMODATION
+      var accomOpt1 = "Accomodation Option 1: " + '\n' + $('input[name="option1Name"]').val() + '\n' 
+      + "Cost: " + $('input[name="option1Cost"]').val() + '\n' + "Rooms: " + $('input[name="option1Rooms"]').val() + '\n' + "Features: " + $('textarea[name="option1Features"]').val();
+      var accomOpt2 = "Accomodation Option 2: " + '\n' + $('input[name="option2Name"]').val() + '\n' 
+      + "Cost: "+ $('input[name="option2Cost"]').val() + '\n' + "Rooms: " + $('input[name="option2Rooms"]').val() + '\n' + "Features: " + $('textarea[name="option2Features"]').val();
+      var accomOpt3 = "Accomodation Option 3: " + '\n' + $('input[name="option3Name"]').val() + '\n' 
+      + "Cost: "+ $('input[name="option3Cost"]').val() + '\n' + "Rooms: " + $('input[name="option3Rooms"]').val() + '\n' + "Features: " + $('textarea[name="option3Features"]').val();
+      //PACKING LIST
+      var pl1 = "Travel documents: " + $('input[name="pl-1"]').val();
+      var pl2 = "Hygiene & Medical: " + $('input[name="pl-2"]').val();
+      var pl3 = "Clothing, Footwear & Beauty: " + $('input[name="pl-3"]').val();
+      var pl4 = "Electronics: " + $('input[name="pl-4"]').val();
+      //ITINERARY
+      var dayOne = "Day One:" + $('input[name="first-day"]').val(); 
+      var dayTwo = "Day Two:" + $('input[name="second-day"]').val(); 
+      var dayThree = "Day Three:" + $('input[name="third-day"]').val(); 
+      var dayFour = "Day Four:" + $('input[name="fourth-day"]').val(); 
+      var dayFive = "Day Five:" + $('input[name="fifth-day"]').val(); 
+      var daySix = "Day Six:" + $('input[name="sixth-day"]').val(); 
+      var daySeven = "Day Seven:" + $('input[name="seventh-day"]').val(); 
+
+      //PAGE TITLE
+      doc.setFont("helvetica", "bold");
+      doc.setFontSize(14);
+      doc.text(pageTitle, 100, 20, { align: "center" });
+      doc.setTextColor('#d55b3e')
+      doc.setFont("Dancing Script", "cursive");
+      doc.setFontSize(30);
+      doc.text(tripName, 100, 40, { align: "center" });
+
+      //SECTION TITLES
+      doc.setFontSize(18);
+      doc.setTextColor('#008b81');
+      doc.text("TRIP INFO", 20, 50);
+      doc.setTextColor('#d55b3e')
+      doc.text("TRIP DURATION", 20, 90);
+      doc.setTextColor('#008b81');
+      doc.text("TRANSPORT", 20, 130);
+      doc.setTextColor('#d55b3e')
+      doc.text("ACCOMMODATION", 20, 190);
+      
+      doc.setFontSize(14);
+      doc.setFont("Dancing Script", "cursive");
+      doc.setTextColor(0, 0, 0); 
+      //TRIP INFO
+      doc.text(tripDestination, 30, 60);
+      doc.text(numPeople, 30, 70);
+      doc.text(typeOfTrip, 30, 80);
+      //TRIP DURATION
+      doc.text(tripDuration, 30, 100);
+      doc.text(tripStart,30, 110);
+      doc.text(tripEnd, 30, 120);  
+      //TRANSPORT
+      doc.text(transportGoing, 30, 140);
+      doc.text(transportBooked, 30, 150);
+      doc.text(transportReturn, 30, 160);
+      doc.text(returnBooked, 30, 170);
+      //ACCOM
+      doc.text(accomOpt1, 30, 200);
+      doc.text(accomOpt2, 100, 200);
+      doc.text(accomOpt3, 170, 200);
+      
+      // new page
+      doc.addPage();
+      doc.setFont("helvetica", "bold");
+      doc.setFontSize(14);
+      doc.text(pageTitle, 100, 20, { align: "center" });
+      //SECTION TITLES
+      doc.setFontSize(18);
+      doc.setTextColor('#008b81');
+      doc.text("BUDGET", 20, 30);
+      doc.setTextColor('#d55b3e');
+      doc.text("ITINERARY", 20, 110);
+      doc.setTextColor('#008b81');
+      doc.text("PACKING LIST", 20, 200);
+      
+      doc.setFontSize(14);
+      doc.setFont("Dancing Script", "cursive");
+      doc.setTextColor(0, 0, 0); 
+      //BUDGET
+      doc.text(budget, 30, 40);
+      doc.text(transportCost, 30, 50);
+      doc.text(accomodationCost, 30, 60);
+      doc.text(groceriesCost, 30, 70);
+      doc.text(drinksCost, 30, 80);
+      doc.text(entertainmentCost, 30, 90);
+      doc.text(`per person Ksh: ${costPerPerson}`, 100, 40);
+      doc.text(`per person Ksh: ${transportCostPerPerson}`, 100, 50);
+      doc.text(`per person Ksh: ${accomodationCostPerPerson}`, 100, 60);
+      doc.text(`per person Ksh: ${groceriesCostPerPerson}`, 100, 70);
+      doc.text(`per person Ksh: ${drinksCostPerPerson}`, 100, 80);
+      doc.text(`per person Ksh: ${entertainmentCostPerPerson}`, 100, 90);
+    
+      //ITINERARY
+      doc.text(dayOne, 30, 120);
+      doc.text(dayTwo, 30, 130);
+      doc.text(dayThree, 30, 140);
+      doc.text(dayFour, 30, 150);
+      doc.text(dayFive, 30, 160);
+      doc.text(daySix, 30, 170);
+      doc.text(daySeven, 30, 180);
+      
+      //PACKING  LIST
+      doc.text(pl1, 30, 210);
+      doc.text(pl2, 30, 220);
+      doc.text(pl3 , 30, 230);
+      doc.text(pl4 , 30, 240);
+
+      // new page
+      doc.addPage();
+      doc.setFont("helvetica", "bold");
+      doc.setFontSize(14);
+      doc.text(pageTitle, 100, 20, { align: "center" });
+      doc.setFontSize(18);
+      doc.setTextColor('#d55b3e')
+      doc.text("CONTACT INFORMATION", 20, 40);
+
+
+      doc.setFontSize(14);
+      doc.setFont("Dancing Script", "cursive");
+      doc.setTextColor(0, 0, 0); 
+      //CONTACT LIST
+      doc.text(contactsList, 30, 50);
+
+      doc.setFontSize(20);
+      doc.setTextColor(128, 128, 128);
+      doc.text("Remember to book on time!", 100, 200, { align: "center" });
+      doc.save("outOfTheGroupChat.pdf");
+      
+  });
